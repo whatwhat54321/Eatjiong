3de38d37fba135898d21aeabddf4103c3353aa62
what54321 <107264584+what54321@users.noreply.github.com>
2022-06-11 19:47:30 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -502,9 +502,9 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 2) return I18N['text-level-1'];
-        if (cps <= 4) return I18N['text-level-2'];
-        if (cps <= 6)  return I18N['text-level-3'];
+        if (cps <= 3) return I18N['text-level-1'];
+        if (cps <= 5) return I18N['text-level-2'];
+        if (cps <= 7)  return I18N['text-level-3'];
         if (cps <= 10) return I18N['text-level-4'];
         return I18N['text-level-5'];
     }
